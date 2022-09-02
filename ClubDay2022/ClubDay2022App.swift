f56db608f4c3b271767df91ea44b7667f674cfe8
Zheng <aheze@getfind.app>
2022-09-01 19:19:45 -0700

    Initial Commit

---
+++ ClubDay2022/ClubDay2022App.swift
@@ -0,0 +1,19 @@
+//
+//  ClubDay2022App.swift
+//  ClubDay2022
+//
+//  Created by A. Zheng (github.com/aheze) on 9/1/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+@main
+struct ClubDay2022App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClubDay2022/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  ClubDay2022
+//
+//  Created by A. Zheng (github.com/aheze) on 9/1/22.
+//  Copyright © 2022 A. Zheng. All rights reserved.
+//
+    
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
